7395fa31f247c08058a9f0b7bb85406675e4eea4
luka dodelia <luka@picktek.com>
2015-12-02 13:52:34 +0400

    all work places settled waiting for lashas final jsons

--- src/Controller/AuditorController.php
+++ src/Controller/AuditorController.php
@@ -7,27 +7,46 @@
 
 namespace Drupal\html_auditor\Controller;
 
+use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Component\Serialization\Json;
+use Drupal\Core\Extension\InfoParser;
 
 class AuditorController extends ControllerBase {
   /**
    * {@inheritdoc}
    */
   public function report() {
+
     // Get config.
     $config = $this->config('html_auditor.settings');
     // Get report directory path.
     $directory = drupal_realpath(sprintf('public://%s', $config->get('sitemap.reports')));
     // Get files from report directory.
-    $files = file_scan_directory($directory, '/[a-z0-9]+\-report.json$/', array(
-      'callback' => function($file) {
-	  		    debug(json_decode(file_get_contents($file)));
-      } 
-    ));
-    $build = array(
-      '#type' => 'markup',
-      '#markup' => t('Hello World!'),
-    );
+    $files = file_scan_directory($directory, '/[a-z0-9]+\-report.json$/');
+
+    $report = [];
+
+    foreach ($files as $file) {
+      $json = file_get_contents($file->uri);
+      $report_data = Json::decode($json);
+      $report_data['name'] = $file->name;
+      $report[] = $report_data;
+    }
+
+    debug($report);
+
+    $build = [
+      '#theme' => 'report',
+      '#reports' => $report,
+      '#name' => 'luka',
+      '#attached' => [
+        'library' => [
+          'html_auditor/reporting-lib'
+        ]
+      ]
+    ];
+
     return $build;
   }
 
