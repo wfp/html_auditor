fc0fd1bcc717d2f438a8559e716092de84919819
Lasha <lashab@picktek.com>
2015-12-10 08:39:12 +0000

    Validate configuration form #2

--- src/Form/AuditorConfigurationForm.php
+++ src/Form/AuditorConfigurationForm.php
@@ -9,6 +9,8 @@ namespace Drupal\html_auditor\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Component\Utility\UrlHelper;
+use Drupal\Component\Utility\Xss;
 
 /**
  * Defines a form that configures html_auditor module settings.
@@ -41,18 +43,21 @@ class AuditorConfigurationForm extends ConfigFormBase {
       '#title' => $this->t('Path to XML sitemap'),
       '#default_value' => $config->get('sitemap.uri'),
       '#size' => 40,
+      '#required' => TRUE,
     );
     $form['sitemap_files'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Downloaded HTML sitemaps directory name or path'),
       '#default_value' => $config->get('sitemap.files'),
       '#size' => 40,
+      '#required' => TRUE,
     );
     $form['sitemap_reports'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Reported HTML sitemaps directory name or path'),
       '#default_value' => $config->get('sitemap.reports'),
       '#size' => 40,
+      '#required' => TRUE,
     );
     $form['a11y_standard'] = array(
       '#type' => 'radios',
@@ -63,6 +68,7 @@ class AuditorConfigurationForm extends ConfigFormBase {
         'WCAG2AAA' => 'WCAG2AAA',
       ),
       '#default_value' => $config->get('a11y.standard'),
+      '#required' => TRUE,
     );
     $form['a11y_ignore'] = array(
       '#type' => 'checkboxes',
@@ -79,12 +85,6 @@ class AuditorConfigurationForm extends ConfigFormBase {
       '#title' => $this->t('HTML5 audit errors only'),
       '#default_value' => $config->get('html5.errors_only'),
     );
-    $form['link_base_uri'] = array(
-      '#type' => 'textfield',
-      '#title' => $this->t('Link audit base URI'),
-      '#default_value' => $config->get('link.base_uri'),
-      '#size' => 40,
-    );
     $form['link_report_verbose'] = array(
       '#type' => 'checkbox',
       '#title' => $this->t('Link audit report verbose'),
@@ -100,14 +100,23 @@ class AuditorConfigurationForm extends ConfigFormBase {
     $values = $form_state->getValues();
     $this->config('html_auditor.settings')
       ->set('sitemap.uri', $values['sitemap_uri'])
-      ->set('sitemap.files', $values['sitemap_files'])
-      ->set('sitemap.reports', $values['sitemap_reports'])
+      ->set('sitemap.files', Xss::filter($values['sitemap_files']))
+      ->set('sitemap.reports', Xss::filter($values['sitemap_reports']))
       ->set('a11y.standard', $values['a11y_standard'])
       ->set('a11y.ignore', $values['a11y_ignore'])
       ->set('html5.errors_only', $values['html5_errors_only'])
-      ->set('link.base_uri', $values['link_base_uri'])
       ->set('link.report_verbose', $values['link_report_verbose'])
       ->save();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $sitemaps_uri = $form_state->getValue('sitemap_uri');
+    if (!UrlHelper::isValid($sitemaps_uri, TRUE) ) {
+      $form_state->setErrorByName('sitemap_uri', $this->t('URL is not valid.'));
+    }
   }
 
 }
